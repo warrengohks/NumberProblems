bc83e0112c49b01bd4b2d66c53d6cd71268c09f4
Joshua Howland <development@jkhowland.com>
2014-08-14 23:50:05 -0600

    Adds Swift and Objective-C Projects

---
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  NumberProblems-Objc
+//
+//  Created by Joshua Howland on 8/14/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+            
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    // Insert code here...
+    
+    
+    return YES;
+}
+
+@end
