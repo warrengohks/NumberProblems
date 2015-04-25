06d0d3658453218694ff975d20e7be3b4df1ffc2
Warren Goh <warrengohkscode@gmail.com>
2015-04-25 15:20:53 -0600

    Finished Project

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -8,19 +8,57 @@
 
 #import "AppDelegate.h"
 
+static int multiple = 8;
+static int divisor = 6;
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
-            
+
+
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // Insert code here...
-    
+    [self iterateCount: 4];
+    [self multiplied:5];
     
     return YES;
 }
+
+- (void)iterateCount:(float)number{
+    
+
+    //Method should call itself and decrement the integer until the value is 0
+    
+    //In the iterateCount method call multiplied and store the return value in a new variable.
+    
+    float newInt = [self multiplied:number];
+    NSLog (@"Multiplied %f by %d to be %f", number, multiple, newInt);
+    
+    float newIntDivide = [self divided:newInt];
+    NSLog (@"divided %f by %d to be %f", number, divisor, newIntDivide);
+    
+    
+    if (number == 0){
+        return;
+    }
+    else{
+        NSLog(@"iterate %f", number);
+        number --;
+        [self iterateCount:number];
+    }
+}
+
+- (float)multiplied:(float)number{
+    return number * multiple;
+}
+
+- (float)divided: (float)number{
+    return number / divisor;
+}
+
 
 @end
